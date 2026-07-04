dab880389d4f553985689c22530411d7c8e10211
Quang Long <dquanglong682005@gmail.com>
2026-07-04 12:07:15 +0700

    feat: add register page and reusable footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import LoginPage from './pages/LoginPage';
-
+import Footer from './components/Footer.tsx';
+import RegisterPage from './pages/RegisterPage';
 function App() {
-  return <LoginPage />;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <RegisterPage />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
