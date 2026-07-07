5c5e354775e7cbe5e313f060b1dade26cdf46253
tinacoding-me <tiennhat110505@gmail.com>
2026-07-07 17:20:25 +0700

    fix: build lint fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes';
-import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
