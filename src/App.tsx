6b8a0cd31df148948f835eb8addac61553df8fb3
tinacoding-me <tiennhat110505@gmail.com>
2026-07-07 17:17:14 +0700

    feat: setup analytics of vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <BrowserRouter>
         <AppRoutes />
+        <Analytics />
       </BrowserRouter>
     </div>
   );
